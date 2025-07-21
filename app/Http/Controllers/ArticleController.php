8b4dca63da171841116471219f5d9a87c7003e3e
CarlosBC14 <carlosbadillac@outlook.com>
2025-07-21 17:09:27 -0400

    Implementación de los Requests para las validaciones de artículos

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreArticleRequest;
+use App\Http\Requests\UpdateArticleRequest;
 use App\Http\Resources\JsonApiCollection;
 use App\Http\Resources\JsonApiResource;
 use App\Models\Article;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class ArticleController extends Controller
@@ -26,25 +27,9 @@ class ArticleController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): JsonApiResource
+    public function store(StoreArticleRequest $request): JsonApiResource
     {
-        $request->validate([
-            'data.type' => ['in:articles'],
-        ]);
-
-        $validated = $request->validate([
-            'data.attributes.title' => ['required', 'string', 'min:4', 'max:255'],
-            'data.attributes.slug' => [
-                'required',
-                'string',
-                'max:255',
-                'regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/',
-                'unique:' . Article::class . ',slug',
-            ],
-            'data.attributes.content' => ['required', 'string'],
-        ]);
-
-        $attributes = $validated['data']['attributes'];
+        $attributes = $request->validated()['data']['attributes'];
         $attributes['user_id'] = $request->user()->id;
 
         $article = Article::create($attributes);
@@ -63,26 +48,9 @@ class ArticleController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Article $article): JsonApiResource
+    public function update(UpdateArticleRequest $request, Article $article): JsonApiResource
     {
-        $request->validate([
-            'data.id' => ['in:' . $article->getRouteKey()],
-            'data.type' => ['in:articles'],
-        ]);
-
-        $validated = $request->validate([
-            'data.attributes.title' => ['sometimes', 'string', 'min:4', 'max:255'],
-            'data.attributes.slug' => [
-                'sometimes',
-                'string',
-                'max:255',
-                'regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/',
-                'unique:' . Article::class . ',slug,' . $article->id,
-            ],
-            'data.attributes.content' => ['sometimes', 'string'],
-        ]);
-
-        $attributes = $validated['data']['attributes'];
+        $attributes = $request->validated()['data']['attributes'];
 
         $article->update($attributes);
 
